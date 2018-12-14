7c1a0d8c9559d9c8dbf88a2bb9d2d1e0225bb1dd
yj <monster2071@163.com>
2018-12-14 10:20:55 +0800

    ignore

--- cast/Monster/DbConsole/Program.cs
+++ cast/Monster/DbConsole/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace DbConsole
 {
@@ -6,7 +8,45 @@ namespace DbConsole
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("Hello World!");
+
+      dynamic num = "test";
+
+      if (!(num is int iNum))
+      {
+        Console.WriteLine("no int");
+      }
+      else
+      {
+        Console.WriteLine(iNum);
+      }
+
+      int[] arr = new[] {1, 2, 3, 4, 5, 6, 7, 8, 9,};
+
+      var list = new List<int>();
+
+      for (int i = 0; i < 1000; i++)
+      {
+        list.Add(i);
+      }
+
+      var readData = ReadData();
+
+      Console.WriteLine("-----------");
+
+      var ints = readData;
+      
+      Console.ReadKey(true);
+
+    }
+
+    public static IEnumerable<int> ReadData()
+    {
+      for (int i = 0; i < 10; i++)
+      {
+        Console.WriteLine($"return {i}");
+        yield return i;//参破延时加载...
+      }
+      yield break;
     }
   }
 }
