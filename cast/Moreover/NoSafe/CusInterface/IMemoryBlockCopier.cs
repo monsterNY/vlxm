b2254bfdc574ac15c21bf4175367e44f544c36b0
yj <monster2071@163.com>
2019-02-18 18:06:16 +0800

    no safe init_two

---
+++ cast/Moreover/NoSafe/CusInterface/IMemoryBlockCopier.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NoSafe.CusInterface
+{
+  public interface IMemoryBlockCopier
+  {
+    void Copy<T>(T[] source, T[] destination);
+    void Copy<T>(T[] source, int sourceStartIndex, T[] destination, int destinationStartIndex, int elementsCount);
+    unsafe void Copy<T>(void* source, void* destination, int elementsCount);
+    unsafe void Copy<T>(void* source, int sourceStartIndex, void* destination, int destinationStartIndex, int elementsCount);
+    unsafe void Copy<T>(void* source, int sourceLength, T[] destination);
+    unsafe void Copy<T>(void* source, int sourceStartIndex, T[] destination, int destinationStartIndex, int elementsCount);
+  }
+}
