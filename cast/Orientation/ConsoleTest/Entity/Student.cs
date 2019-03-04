0a3ad7a0617e20341be60afb118cf0e523538c95
yj <monster2071@163.com>
2019-03-04 17:21:28 +0800

    empty

---
+++ cast/Orientation/ConsoleTest/Entity/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleTest.Entity
+{
+  public class Student
+  {
+
+    public int Age { get; set; }
+
+    public string Name { get; set; }
+
+    public int ClassId { get; set; }
+
+  }
+}
---
+++ cast/Orientation/ConsoleTest/Program.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using ConsoleTest.Entity;
+using Tools.RefTools;
+
+namespace ConsoleTest
+{
+  class Program
+  {
+
+    public static dynamic Build<T>(T instance, string[] properties)
+    {
+
+      dynamic obj = new System.Dynamic.ExpandoObject();
+
+      var type = typeof(T);
+
+      foreach (var prop in properties)
+      {
+        var propertyInfo = type.GetProperty(prop);
+
+        if (propertyInfo != null)
+        {
+          ((IDictionary<string, object>)obj).Add(propertyInfo.Name, propertyInfo.GetValue(instance));
+        }
+
+      }
+
+      return obj;
+
+    }
+
+    static void Main(string[] args)
+    {
+
+      var stu = new Student()
+      {
+        Age = 18,
+        ClassId = 1,
+        Name = "xxx"
+      };
+
+      var result = Build(stu, new[] {nameof(Student.Age), nameof(Student.Name)});
+
+      Console.WriteLine(result.Age);
+      Console.WriteLine(result.Name);
+
+      Console.WriteLine("Hello World!");
+
+      Console.ReadKey(true);
+    }
+
+    private static void GroupCount()
+    {
+      var testCount = 10;
+
+      var arr = new List<dynamic>();
+
+      var rand = new Random();
+
+      for (int i = 0; i < 1000000; i++)
+      {
+        arr.Add(new { sex = rand.Next(2), age = rand.Next(100) });
+      }
+
+      var timer = new CodeTimer();
+
+      var result = timer.Time(testCount, (() =>
+      {
+        //        Console.WriteLine("-------------------------linq-----------------------");
+        var enumerable = arr.GroupBy(u => u.sex).Select(u => new { u.Key, count = u.Count() });
+        foreach (var item in enumerable)
+        {
+          //          Console.WriteLine($"性别：{(item.Key == 0 ? "女" : "男")}，数量：{item.count}");
+        }
+
+        //
+        //        Console.WriteLine();
+      }));
+
+      Console.WriteLine("-------------------------linq-----------------------"); //效率最低。。。
+      Console.WriteLine(result);
+
+      result = timer.Time(testCount, (() =>
+      {
+        var countArr = new int[2];
+
+        foreach (var item in arr)
+        {
+          countArr[item.sex == 0 ? 0 : 1]++;
+        }
+
+        foreach (var item in countArr)
+        {
+        }
+
+        //        Console.WriteLine("-------------------------arr-----------------------");
+        //        Console.WriteLine($"性别男，数量：{countArr[1]}");
+        //        Console.WriteLine($"性别女，数量：{countArr[0]}");
+        //        Console.WriteLine();
+      }));
+
+      Console.WriteLine("-------------------------arr-----------------------"); //arr效率最高
+      Console.WriteLine(result);
+
+      result = timer.Time(testCount, (() =>
+      {
+        //        Console.WriteLine("-------------------------dictionary-----------------------");
+        var countDic = new Dictionary<int, int>();
+
+        foreach (var item in arr)
+        {
+          if (countDic.ContainsKey(item.sex))
+          {
+            countDic[item.sex]++;
+          }
+          else
+          {
+            countDic.Add(item.sex, 1);
+          }
+        }
+
+        foreach (var item in countDic)
+        {
+          //          Console.WriteLine($"性别：{(item.Key == 0 ? "女" : "男")}，数量：{item.Value}");
+        }
+
+        //
+        //        Console.WriteLine();
+      }));
+
+      Console.WriteLine("-------------------------dictionary-----------------------"); //字典效率第二
+      Console.WriteLine(result);
+    }
+  }
+}
